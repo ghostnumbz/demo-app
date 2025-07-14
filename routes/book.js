a13d2fcd5d56dfc3d2455afb80f138f2fd2d4102
unknown <yaswanth.kumar.it@outlook.com>
2025-07-14 14:38:58 -0700

    some chages in ejs

--- routes/book.js
+++ routes/book.js
@@ -2,9 +2,9 @@ const express = require("express")
 const router = express.Router()
 const bookController = require("../controllers/book")
 
-router.get('/books',bookController.handlegetAllBooks);
-router.get('/books/:bookId',bookController.handelgetBookBYId);
-router.post('/book',bookController.handleCreateNewBook);
-router.delete('/book/:bookid',bookController.handleDeleteBookById);
+router.get('/books',bookController.handlegetAllBooks);// get all books
+router.get('/books/:bookId',bookController.handelgetBookBYId);// get book by ID
+router.post('/book',bookController.handleCreateNewBook);// create a new book
+router.delete('/book/:bookid',bookController.handleDeleteBookById);// delete a book by ID
 
 module.exports= router;
